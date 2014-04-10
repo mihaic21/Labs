a92e53bbb9cf3f857e4622346a97a9366aa21ea9
Shobo <mihai.co21@yahoo.com>
2014-04-10 18:48:09 +0300

    [DB] Lab1

---
+++ An2/Sem2/DB/DB-Lab1/DB-Lab1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DB_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(379, 275);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(397, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(431, 275);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(250, 386);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(446, 385);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 3;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(351, 434);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 484);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ An2/Sem2/DB/DB-Lab1/DB-Lab1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Lab1
+{
+    public partial class Form1 : Form
+    {
+        private String connectionString = "Data Source=(local);Initial Catalog=Sports Manager;Integrated Security=SSPI;";
+        private SqlDataAdapter daSports;
+        private SqlDataAdapter daLeagues;
+        private DataSet dataSet;
+        private DataRelation dr;
+        private SqlCommandBuilder commandBuilder;
+        private BindingSource bsSports = new BindingSource();
+        private BindingSource bsLeagues = new BindingSource();
+        private SqlConnection dbConn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = bsSports;
+            this.dataGridView2.DataSource = bsLeagues;
+            this.getData();
+        }
+
+        private void getData()
+        {
+            this.dbConn = new SqlConnection(connectionString);
+            this.dataSet = new DataSet();
+            this.daSports = new SqlDataAdapter("SELECT * FROM SPORTS", this.dbConn);
+            this.daLeagues = new SqlDataAdapter("SELECT * FROM LEAGUES", this.dbConn);
+            this.commandBuilder = new SqlCommandBuilder(this.daLeagues);
+            
+            this.daSports.Fill(this.dataSet, "Sports");
+            this.daLeagues.Fill(this.dataSet, "Leagues");
+
+            this.dr = new DataRelation("SportsLeagues", this.dataSet.Tables["Sports"].Columns["League_ID"], this.dataSet.Tables["Leagues"].Columns["Sport_ID"]);
+            this.dataSet.Relations.Add(this.dr);
+
+            this.bsSports.DataSource = this.dataSet;
+            this.bsSports.DataMember = "Sports";
+
+            this.bsLeagues.DataSource = this.bsSports;
+            this.bsLeagues.DataMember = "SportsLeagues";
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView2.CurrentRow != null)
+                {
+                    this.dataGridView2.Rows.RemoveAt(this.dataGridView2.CurrentRow.Index);
+                    this.daLeagues.Update(this.dataSet, "Leagues");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.daLeagues.Update(this.dataSet, "Leagues");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+    }
+}
